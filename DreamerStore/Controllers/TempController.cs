47f5da3373bb34ac6b8e80587ff6d0377760f292
Anh Dao Nguyen Thi <52000830@student.tdtu.edu.vn>
2023-10-13 20:25:05 +0700

    Model

---
+++ DreamerStore/Controllers/TempController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DreamerStore.Models;
+
+namespace DreamerStore.Controllers
+{
+    public class TempController : Controller
+    {
+        DreamerStoreEntities _db = new DreamerStoreEntities();
+        // GET: Temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getMenu()
+        {
+            var v = from t in _db.menus
+                    where t.hide == true
+                    orderby t.order ascending
+                    select t;
+            return PartialView(v.toList());
+        }
+    }
+}
